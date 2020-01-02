974597d72fca1ef7ffd789761b9adfc66763d6ea
hex@umonkey.net <hex@umonkey.net>
2020-01-02 19:36:11 +0300

    Moved css/js to css/js folders

--- themes/land/assets.php
+++ themes/land/assets.php
@@ -1,23 +1,23 @@
 <?php
 
 return [
-    'public/app.css' => [
+    'public/css/app.css' => [
         './vendor/umonkey/ufw1/assets/*.css',
         'css/*.css',
     ],
 
-    'public/app.js' => [
+    'public/js/app.js' => [
         './vendor/umonkey/ufw1/assets/*.js',
         'js/js/*.js',
     ],
 
-    'public/libs.css' => [
+    'public/css/libs.css' => [
         'fonts/pt-sans.css',
         'fonts/fontawesome.css',
         'lib/fancybox/jquery.fancybox.min.css',
     ],
 
-    'public/libs.js' => [
+    'public/js/libs.js' => [
         'lib/jquery/jquery-3.3.1.min.js',
         'lib/fancybox/jquery.fancybox.min.js',
     ],
