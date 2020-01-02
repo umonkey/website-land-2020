da496a366120cb438a7f221566b76192b0b3ab36
hex@umonkey.net <hex@umonkey.net>
2020-01-02 21:59:34 +0300

    Fixed reading wiki pages by links

--- src/App/Handlers/Wiki.php
+++ src/App/Handlers/Wiki.php
@@ -16,15 +16,17 @@ class Wiki extends \Ufw1\Handlers\Wiki
 {
     public function onDefault(Request $request, Response $response, array $args)
     {
-        $url = '/' . $args['path'];
+        $url = '/' . $args['url'];
 
         $id = $this->db->fetchcell('SELECT `id` FROM `nodes_wiki_idx` WHERE `url` = ?', [$url]);
-        if (empty($id))
+        if (empty($id)) {
             $this->notfound();
+        }
 
         $node = $this->node->get($id);
-        if (empty($node) or $node['type'] != 'wiki')
+        if (empty($node) or $node['type'] != 'wiki') {
             $this->notfound();
+        }
 
         $name = $node['name'];
         return $this->showPageByName($request, $response, $name);
