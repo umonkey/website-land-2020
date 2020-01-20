c5c08fbff954fdfcc98b27a9396d1d002b430396
hex@umonkey.net <hex@umonkey.net>
2020-01-20 15:53:18 +0300

    errorHandler update

--- config/dependencies.php
+++ config/dependencies.php
@@ -13,3 +13,10 @@ Util::installErrorHandler($container);
 $container['kdpv'] = function ($c) {
     return new \App\Services\NodePictureService($c);
 };
+
+$container['errorHandler'] = function ($c) {
+    return function ($request, $response, $e) use ($c) {
+        $h = new \App\Handlers\Error($c);
+        return $h($request, $response, ['exception' => $e]);
+    };
+};
