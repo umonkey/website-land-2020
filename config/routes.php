8fa2fc5f85c7c14d2ac31c7bc1bc587aa4369956
hex@umonkey.net <hex@umonkey.net>
2020-01-20 11:02:49 +0300

    Clean up route config

--- config/routes.php
+++ config/routes.php
@@ -9,27 +9,6 @@ use Slim\Http\Response;
 \App\Handlers\Wiki::setupRoutes($app);
 \Ufw1\Handlers\Files::setupRoutes($app);
 
-$app->add(function ($request, $response, $next) use ($app) {
-    if ($request->getMethod() == 'GET') {
-        $path = $request->getUri()->getPath();
-
-        $db = $app->getContainer()->get('database');
-        $dst = $db->fetchcell('SELECT node_id FROM rewrite WHERE src = ?', [$path]);
-
-        if (!empty($dst)) {
-            $env = \Slim\Http\Environment::mock([
-                'REQUEST_METHOD' => 'GET',
-                'REQUEST_URI' => "/node/{$dst}.html",
-            ]);
-
-            $request = Request::createFromEnvironment($env);
-        }
-    }
-
-    $response = $next($request, $response);
-    return $response;
-});
-
 $app->get ('/', '\App\Handlers\Home:onHome');
 $app->get ('/articles', '\App\Handlers\Articles:onList');
 $app->get ('/blog', '\App\Handlers\Blog:onBlog');
@@ -40,8 +19,8 @@ $app->get ('/files/recent.json', '\App\Handlers\Files:onGetRecentJson');
 $app->get ('/files/{id:[0-9]+}', '\App\Handlers\Files:onShowFile');
 $app->get ('/files/{id:[0-9]+}/download', '\App\Handlers\Files:onDownload');
 $app->get ('/f/{name}', '\App\Handlers\Storage:onGetItem');
-$app->get ('/search', \App\Handlers\Search::class . ':onGet');
-$app->get ('/search/log', \App\Handlers\Search::class . ':onLog');
+$app->get ('/search', '\App\Handlers\Search:onGet');
+$app->get ('/search/log', '\App\Handlers\Search:onLog');
 $app->get ('/sitemap.xml', '\App\Handlers\Sitemap');
 $app->get ('/stats', '\App\Handlers\Home:onStats');
 $app->get ('/node/{id:[0-9]+}', '\App\Handlers\Node:onShow');
