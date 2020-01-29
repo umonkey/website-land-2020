5666bdedbd68041eb9107973440ec551df81bd74
hex@umonkey.net <hex@umonkey.net>
2020-01-29 16:25:57 +0300

    Properly reload disqus comments

--- themes/land/js/600-disqus.js
+++ themes/land/js/600-disqus.js
@@ -1,27 +1,39 @@
+/**
+ * Загрузка комментариев к записи.
+ *
+ * По ходу навигации используем DISQUS.reset:
+ * https://help.disqus.com/en/articles/1717163-using-disqus-on-ajax-sites
+ **/
 jQuery(function ($) {
-    var w = window;
-
-    var start = function () {
-        if ($('#disqus_thread').length > 0) {
-            w.disqus_shortname = 'umonkey-land';
-
-            var did = w.jsdata.disqus_id || null;
-            var durl = w.jsdata.disqus_url || null;
-
-            if (did) {
-                w.disqus_identifier = did;
-            } else if (durl) {
-                w.disqus_url = durl;
-            }
-
-            $.getScript('https://umonkey-land.disqus.com/embed.js', function () {
-                console && console.log('disqus comments loaded');
-            });
+    var disqus_check = function (callback) {
+        var thr = $('#disqus_thread');
+        if (thr.length == 1) {
+            var did = thr.data('id');
+            callback(did);
         } else {
             console && console.log('disqus comments disabled ');
         }
     };
 
-    start();
-    $(document).on('ufw:reload', start);
+    var w = window;
+
+    disqus_check(function (disqus_id) {
+        w.disqus_shortname = 'umonkey-land';
+        w.disqus_identifier = disqus_id;
+
+        $.getScript('https://umonkey-land.disqus.com/embed.js', function () {
+            console && console.log('disqus comments loaded');
+        });
+    });
+
+    $(document).on('ufw:reload', function () {
+        disqus_check(function (disqus_id) {
+            DISQUS.reset({
+                reload: true,
+                config: function () {
+                    this.page.identifier = disqus_id;
+                }
+            });
+        });
+    });
 });
