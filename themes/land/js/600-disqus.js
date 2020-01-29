bb6c74062c787db0cf56bf376ac1271f40d86fda
hex@umonkey.net <hex@umonkey.net>
2020-01-29 16:07:42 +0300

    Reinitialize comments

--- themes/land/js/600-disqus.js
+++ themes/land/js/600-disqus.js
@@ -1,22 +1,27 @@
 jQuery(function ($) {
     var w = window;
 
-    if ($('#disqus_thread').length > 0) {
-        w.disqus_shortname = 'umonkey-land';
+    var start = function () {
+        if ($('#disqus_thread').length > 0) {
+            w.disqus_shortname = 'umonkey-land';
 
-        var did = w.jsdata.disqus_id || null;
-        var durl = w.jsdata.disqus_url || null;
+            var did = w.jsdata.disqus_id || null;
+            var durl = w.jsdata.disqus_url || null;
 
-        if (did) {
-            w.disqus_identifier = did;
-        } else if (durl) {
-            w.disqus_url = durl;
+            if (did) {
+                w.disqus_identifier = did;
+            } else if (durl) {
+                w.disqus_url = durl;
+            }
+
+            $.getScript('https://umonkey-land.disqus.com/embed.js', function () {
+                console && console.log('disqus comments loaded');
+            });
+        } else {
+            console && console.log('disqus comments disabled ');
         }
+    };
 
-        $.getScript('https://umonkey-land.disqus.com/embed.js', function () {
-            console && console.log('disqus comments loaded');
-        });
-    } else {
-        console && console.log('disqus comments disabled ');
-    }
+    start();
+    $(document).on('ufw:reload', start);
 });
